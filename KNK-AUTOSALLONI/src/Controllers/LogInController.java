c116a46b14e10d22a81ba836c917d51a3f3269f4
Çlirim <53019723+clirimhodaj@users.noreply.github.com>
2020-07-04 03:47:02 +0200

    Update LogInController.java

--- KNK-AUTOSALLONI/src/Controllers/LogInController.java
+++ KNK-AUTOSALLONI/src/Controllers/LogInController.java
@@ -1,14 +1,10 @@
 package Controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.sql.Connection;
@@ -18,127 +14,113 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import DBConnection.DBHandler;
-
+import Controllers.HomepageController;
 
 public class LogInController implements Initializable {
 
-    @FXML
-    private TextField lblusername;
+	HomepageController home = new HomepageController();
 
-    @FXML
-    private Button loginbtn;
+	@FXML
+	private TextField lblusername;
 
-    @FXML
-    private Button signupbutton;
+	@FXML
+	private Button loginbtn;
 
-    @FXML
-    private PasswordField lblpassword;
+	@FXML
+	private Button signupbutton;
 
-    @FXML
-    private Label errorlabel;
+	@FXML
+	private PasswordField lblpassword;
 
-    private DBHandler handler;
-    private Connection connection;
-    private PreparedStatement pst;
-    private static LogInController instance;
+	@FXML
+	private Label errorlabel;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+	private DBHandler handler;
+	private Connection connection;
+	private PreparedStatement pst;
+	private static LogInController instance;
 
-        handler = new DBHandler();
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 
-    public LogInController()
-    {
-        instance = this;
-    }
+		handler = new DBHandler();
+	}
 
-    public static LogInController getInstance()
-    {
-        return instance;
-    }
+	public LogInController()
+	{
+		instance = this;
+	}
 
-
-    public String username()
-    {
-        return lblusername.getText();
-    }
-
-    @FXML
-    public void loginAction(ActionEvent actionEvent) throws Exception {
-
-        String email = lblusername.getText().trim();
-        String password = lblpassword.getText().trim();
-
-        if (email.isEmpty() || password.isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error!");
-            alert.setContentText("Type Username and Password");
-            alert.showAndWait();
-        }
+	public static LogInController getInstance()
+	{
+		return instance;
+	}
 
 
-        connection = handler.getConnection();
-        String q1 = "SELECT * FROM users where emri=?";
+	public String username()
+	{
+		return lblusername.getText();
+	}
 
-        try {
-            pst = connection.prepareStatement(q1);
-            pst.setString(1, lblusername.getText());
+	@FXML
+	public void loginAction(ActionEvent actionEvent) throws Exception {
 
-            ResultSet rs = pst.executeQuery();
+		String email = lblusername.getText().trim();
+		String password = lblpassword.getText().trim();
 
-            if (rs.next()) {
-                if (SignUpController.check(lblpassword.getText(), rs.getString("fjalkalimi"))) {
-                    // admin - admin123
-                    if(rs.getString("roli").equals("a")){
-                        loginbtn.getScene().getWindow().hide();
-                        Stage loginbtn1 = new Stage();
-                        Parent root = FXMLLoader.load(getClass().getResource("../FXML/AdminPage.fxml"));
-                        Scene scene1 = new Scene(root);
-//                        scene1.getStylesheets().add(getClass().getResource("../FXML/button.css").toExternalForm());
-                        loginbtn1.setScene(scene1);
-                        loginbtn1.show();
-                        loginbtn1.setResizable(true);
-                        return;
-                    }
-                    loginbtn.getScene().getWindow().hide();
-                    Stage home = new Stage();
-                    Parent root = FXMLLoader.load(getClass().getResource("../FXML/HomePageMain.fxml"));
-                    Scene scene = new Scene(root);
-                    home.setScene(scene);
-                    home.show();
-                } else {
-                    errorlabel.setTextFill(Color.RED);
-                    errorlabel.setText("Enter correct Username/Password");
-                }
-            } else {
-                errorlabel.setTextFill(Color.RED);
-                errorlabel.setText("Enter correct Username/Password");
-            }
+		if (email.isEmpty() || password.isEmpty()) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error!");
+			alert.setContentText("Type Username and Password");
+			alert.showAndWait();
+		}
 
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		connection = handler.getConnection();
+		String q1 = "SELECT * FROM users where emri=?";
 
-    @FXML
-    public void signup(ActionEvent actionEvent) throws Exception {
-        loginbtn.getScene().getWindow().hide();
+		try {
+			pst = connection.prepareStatement(q1);
+			pst.setString(1, lblusername.getText());
 
-        Stage signupbutton = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML/SignUpMain.fxml"));
-        Scene scene = new Scene(root);
-        signupbutton.setScene(scene);
-        signupbutton.show();
-        signupbutton.setResizable(false);
-    }
+			ResultSet rs = pst.executeQuery();
+
+			if (rs.next()) {
+				if (SignUpController.check(lblpassword.getText(), rs.getString("fjalkalimi"))) {
+					// admin - admin123
+					if(rs.getString("roli").equals("a")){
+						loginbtn.getScene().getWindow().hide();
+						home.loadFxml("../FXML/AdminPage.fxml","Admin");
+						return;
+					}
+					loginbtn.getScene().getWindow().hide();
+					home.loadFxml("../FXML/HomePageMain.fxml","Homepage");
+				} else {
+					errorlabel.setTextFill(Color.RED);
+					errorlabel.setText("Enter correct Username/Password");
+				}
+			} else {
+				errorlabel.setTextFill(Color.RED);
+				errorlabel.setText("Enter correct Username/Password");
+			}
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@FXML
+	public void signup(ActionEvent actionEvent) throws Exception {
+		loginbtn.getScene().getWindow().hide();
+		home.loadFxml("../FXML/SignUpMain.fxml", "Sign Up");
+	}
 
 
 }
